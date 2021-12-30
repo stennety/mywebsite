5f45816ca210a507cb7aebfaf6732f0286031371
Andrew <andrewlee1807@gmail.com>
2021-12-30 17:52:24 +0900

    add content of nav

---
+++ js/nagvigate.js
@@ -0,0 +1,31 @@
+var html_insert_blog = `<ul class="nav-detail-content">
+                                <li><a href="#">Blog 1</a></li>
+                                <li><a href="#">Blog 2</a></li>
+                                <li><a href="#">Blog 3</a></li>
+                                <li><a href="#">Blog 4</a></li>
+                                <li><a href="#">Blog 5</a></li>
+
+                            </ul>`;
+var html_insert_book = `<ul class="nav-detail-content">
+                                <li><a href="#">Cheat Sheets for AI - <i>BecommingHuman.AI</i></a></li>
+                                <li><a href="#">Deep learning (vi) - <i>Tran Thanh Tuan</i> </a></li>
+                            </ul>`;
+var html_insert_project = `<ul class="nav-detail-content">
+                                <li><a href="#">Project 1</a></li>
+                                <li><a href="#">Project 2</a></li>
+                                <li><a href="#">Project 3</a></li>
+                            </ul>`;
+
+
+
+function blog(){
+    document.getElementById("Content_Nav").innerHTML = html_insert_blog;
+}
+
+function book(){
+    document.getElementById("Content_Nav").innerHTML = html_insert_book;
+}
+
+function project(){
+    document.getElementById("Content_Nav").innerHTML = html_insert_project;
+}

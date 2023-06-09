7d862317bb6ac90c26433d9c2220fcb3ab7d76f8
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2023-06-09 01:46:28 +0100

    more config

--- tina/config.ts
+++ tina/config.ts
@@ -7,8 +7,8 @@ const token = process.env.TINA_TOKEN || null;
 
 export default defineConfig({
   branch,
-  token: '<Your Read Only Token>', // generated on app.tina.io,
-  clientId: '<Your Client ID>', // generated on app.tina.io
+  token: token, // generated on app.tina.io,
+  clientId: clientId, // generated on app.tina.io
 
   build: {
     outputFolder: "admin",

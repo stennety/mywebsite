09cf8c83ca018c04ea87323c8847a35b4e97e89e
uotpe <jjason.zhou@gmail.com>
2024-07-19 00:07:58 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://uotpe.github.io',
-  base: 'uotpe'
+  site: 'https://uotpe.github.io'
 });
